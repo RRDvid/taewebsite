594d9a7da061f63fc8cd2d7beebaedba6e69e70a
Kevin David <kevin@jaladesign.com.au>
2021-06-15 21:58:37 +1000

    Addition: MDB library to come from webpack custom build

---
+++ mdb-webpack-starter-master/src/js/index.js
@@ -0,0 +1,96 @@
+//* **** Import All *****/
+// import * as mdb from 'mdb-ui-kit';
+// export default {
+//   mdb,
+// };
+
+//* **** Import Specific *****/
+
+// BOOTSTRAP CORE COMPONENTS
+import Button from '../../mdb/js/free/button';
+import Collapse from '../../mdb/js/bootstrap/mdb-prefix/collapse';
+import Offcanvas from '../../mdb/js/bootstrap/mdb-prefix/offcanvas';
+import Carousel from '../../mdb/js/free/carousel';
+import Popover from '../../mdb/js/free/popover';
+import ScrollSpy from '../../mdb/js/free/scrollspy';
+import Tab from '../../mdb/js/free/tab';
+import Tooltip from '../../mdb/js/free/tooltip';
+
+// MDB FREE COMPONENTS
+import Input from '../../mdb/js/free/input';
+import Dropdown from '../../mdb/js/free/dropdown';
+import Ripple from '../../mdb/js/free/ripple';
+import Range from '../../mdb/js/free/range';
+
+// MDB PRO COMPONENTS
+import Animate from '../../mdb/js/pro/animate';
+import Chart from '../../mdb/js/pro/charts';
+import Lightbox from '../../mdb/js/pro/lightbox';
+import Rating from '../../mdb/js/pro/rating';
+import Sidenav from '../../mdb/js/pro/sidenav';
+import Alert from '../../mdb/js/pro/alert';
+import Toast from '../../mdb/js/pro/toast';
+import Timepicker from '../../mdb/js/pro/timepicker';
+import Navbar from '../../mdb/js/pro/navbar';
+import InfiniteScroll from '../../mdb/js/pro/infinite-scroll';
+import LazyLoad from '../../mdb/js/pro/lazy-load';
+import Datepicker from '../../mdb/js/pro/datepicker';
+import Popconfirm from '../../mdb/js/pro/popconfirm';
+import Datatable from '../../mdb/js/pro/datatable';
+import Stepper from '../../mdb/js/pro/stepper';
+import Sticky from '../../mdb/js/pro/sticky';
+import Select from '../../mdb/js/pro/select';
+import Touch from '../../mdb/js/pro/touch';
+import SmoothScroll from '../../mdb/js/pro/smooth-scroll';
+import PerfectScrollbar from '../../mdb/js/pro/perfect-scrollbar';
+import Loading from '../../mdb/js/pro/loading-management';
+import Autocomplete from '../../mdb/js/pro/autocomplete';
+import Modal from '../../mdb/js/pro/modal';
+import Clipboard from '../../mdb/js/pro/clipboard';
+import ChipsInput from '../../mdb/js/pro/chips';
+import MultiRangeSlider from '../../mdb/js/pro/multi-range';
+import Datetimepicker from '../../mdb/js/pro/date-time-picker';
+
+export {
+  // FREE
+  Button,
+  Carousel,
+  Collapse,
+  Offcanvas,
+  Dropdown,
+  Input,
+  Modal,
+  Popover,
+  ScrollSpy,
+  Ripple,
+  Tab,
+  Tooltip,
+  Range,
+  // PRO
+  Alert,
+  Animate,
+  Chart,
+  Datepicker,
+  Datatable,
+  Lightbox,
+  Navbar,
+  Popconfirm,
+  Rating,
+  Sidenav,
+  SmoothScroll,
+  Timepicker,
+  Toast,
+  InfiniteScroll,
+  LazyLoad,
+  Stepper,
+  Sticky,
+  Select,
+  Touch,
+  PerfectScrollbar,
+  Loading,
+  Autocomplete,
+  Clipboard,
+  ChipsInput,
+  MultiRangeSlider,
+  Datetimepicker,
+};
